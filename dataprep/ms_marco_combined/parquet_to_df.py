fac1157cd4a528828dc885ca75d77f3a11269a9d
dimitar-seraffimov <dimitar.seraffimov@gmail.com>
2025-04-21 17:04:10 +0100

    Generated triplets of queries, relevant (positive) documents and irrelevant (negative) documents (using negative sampling) from the combined dataset. Split the triplets into train (80%), validate (10%) and test splits(10%).

--- dataprep/ms_marco_combined/parquet_to_df.py
+++ dataprep/ms_marco_combined/parquet_to_df.py
@@ -6,10 +6,10 @@ df = pd.read_parquet("combined.parquet")
 # View the first few rows
 #print(df.head("50"))
 
-print(df["passages"])
+print(df[["passages", "query"]])
 
-# Create a new DataFrame with the passages
-passages_df = pd.DataFrame(df["passages"])
+# Create a new DataFrame with the passages and query
+passages_df = pd.DataFrame(df[["passages", "query"]])
 
 # Save the passages DataFrame to a CSV file
-passages_df.to_csv("passages.csv", index=False)
+passages_df.to_csv("passages_and_query.csv", index=False)
